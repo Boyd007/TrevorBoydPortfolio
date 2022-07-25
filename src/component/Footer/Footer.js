70b8e2e24aa0af34dbb6e970e4a98ac7e50915a7
Boyd007 <105176171+Boyd007@users.noreply.github.com>
2022-07-25 14:26:44 -0400

    adjfooter

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -4,10 +4,10 @@ function Footer() {
   <a href="https://github.com/Boyd007" target="_blank">
     <i class="fab fa-github"></i>
   </a>
-  <a href="https://github.com/Boyd007" target="_blank">
+  <a href="https://www.youtube.com/user/BoydMediaNetworks/featured" target="_blank">
   <i class="fab fa-youtube"></i>
   </a>
-  <a href="https://github.com/Boyd007" target="_blank">
+  <a href="https://www.linkedin.com/in/trevor-boyd-65730422/" target="_blank">
   <i class="fab fa-linkedin"></i>
   </a>
   </footer>;
