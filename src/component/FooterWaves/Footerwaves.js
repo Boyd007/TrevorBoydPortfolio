0b2fe673bed230d863a3e3e77b829b347e27bde4
Boyd007 <105176171+Boyd007@users.noreply.github.com>
2022-07-25 13:44:46 -0400

    adjwaves

--- src/component/FooterWaves/Footerwaves.js
+++ src/component/FooterWaves/Footerwaves.js
@@ -1,23 +1,35 @@
 import React from "react";
-import Wave from 'react-wavify'
+import Wave from "react-wavify";
 import "./footerwaves.css";
 
 function Footerwaves() {
   return (
- <div id="footerwaves">
-
-    <Wave mask="url(#mask)" fill="#280137" >
-  <defs>
-    <linearGradient id="gradient" gradientTransform="rotate(90)">
-      <stop offset="0" stopColor="white" />
-      <stop offset="0.5" stopColor="white" />
-    </linearGradient>
-    <mask id="mask">
-      <rect x="0" y="0" width="2000" height="200" fill="url(#gradient)"  />
-    </mask>
-  </defs>
-</Wave>
- </div>
+    <div id="footerwaves">
+      <Wave mask="url(#mask)" fill="#280137">
+        <defs>
+          <linearGradient id="gradient" gradientTransform="rotate(90)">
+            <stop offset="0" stopColor="white" />
+            <stop offset="0.5" stopColor="white" />
+          </linearGradient>
+          <mask id="mask">
+            <rect x="0" y="0" width="2000" height="200" fill="url(#gradient)" />
+          </mask>
+        </defs>
+      </Wave>
+    </div>
   );
 }
+
+// const App = () => (
+//   <Wave fill= '#280137'
+//   paused={false}
+//   options={{
+//     height: 220,
+//    amplitude: 20,
+//     speed: 0.5,
+//     points: 3,
+//   }}
+// />
+// )
+
 export default Footerwaves;
