f03181032b008350116e819cdb13dce283a48289
Boyd007 <105176171+Boyd007@users.noreply.github.com>
2022-07-28 23:59:23 -0400

    finalpush

--- src/component/Portfoliocarousel/Portfoliocarousel.js
+++ src/component/Portfoliocarousel/Portfoliocarousel.js
@@ -3,9 +3,32 @@ import "./portfoliocarousel.scss";
 function Portfoliocarousel (){
   return ( 
     <div className="portfolio-carousel">
-      <p>test test</p>
+      <p>My Portfolio</p>
+
+      <a href="https://github.com/Boyd007/C19JATE">Just Another Text Editor J.A.T.E</a>
+      <a href="https://github.com/Boyd007/C6YourWeatherDash">Your Weather Dashboard</a>
+      <a href="https://github.com/Boyd007/C11NoteTaker">Note Taking Application</a>
+      <a href="https://github.com/Boyd007/C9ProReadMe">README Generator</a>
+      <a href="https://github.com/Boyd007/C5Scheduler">Your Workday Scheduler</a>
+      <a href="https://github.com/Boyd007/C3PasswordGenTrevorBoyd">Password Generator</a>
+
+
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default Portfoliocarousel;
